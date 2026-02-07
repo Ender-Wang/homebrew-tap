c58cee7e82d12470dc76e041c69f48a75a50cebf
Ender-Wang <33310255+Ender-Wang@users.noreply.github.com>
2026-02-07 17:42:38 +0800

    Update sha256 for Swifka cask

---
+++ Casks/swifka.rb
@@ -0,0 +1,13 @@
+cask "swifka" do                                                                                                                    
+  version "0.1"                                                                                                                     
+  sha256 "sha256:eed5c36a12a32b55d80de7d57dafdad1b3511ad31abac9046fac45cfcd1b90a0"                                                                                                        
+                                                                                                                                    
+  url "https://github.com/Ender-Wang/Swifka/releases/download/v#{version}/Swifka-v#{version}.dmg"                                   
+  name "Swifka"                                                                                                                     
+  desc "Native macOS Kafka monitoring client"
+  homepage "https://github.com/Ender-Wang/Swifka"
+
+  depends_on macos: ">= :sequoia"
+
+  app "Swifka.app"
+end
