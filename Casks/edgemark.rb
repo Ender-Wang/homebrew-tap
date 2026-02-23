c3878a265b94d15efaa03996c2f3706285451ca5
Ender Wang <33310255+Ender-Wang@users.noreply.github.com>
2026-02-23 18:52:13 +0800

    setup EdgeMark tap

---
+++ Casks/edgemark.rb
@@ -0,0 +1,17 @@
+cask "edgemark" do
+  version "0.0.0"
+  sha256 "xxxx"
+
+  url "https://github.com/Ender-Wang/EdgeMark/releases/download/v#{version}/EdgeMark-v#{version}.dmg"
+  name "EdgeMark"
+  desc "A SideNotes alternative"
+  homepage "https://github.com/Ender-Wang/EdgeMark"
+
+  depends_on macos: ">= :sequoia"
+
+  app "EdgeMark.app"
+
+  postflight do
+    system_command "/usr/bin/xattr", args: ["-cr", "#{appdir}/EdgeMark.app"]
+  end
+end
